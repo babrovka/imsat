dda23d086a9e8bf45a1856c6f29200622b148de0
babrovka <babrovka@gmail.com>
2012-09-14 21:03:29 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imsat::Application.config.secret_token = 'dd9826f2bc346cd551cbd5726370506148735df9dcfa6844a21469c0bf4b9664aa8f8b9e2a0b90c6b225910bbb2a13a6e2bfb63179a6bac8981ed39d2e5ca497'
