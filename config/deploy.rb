116fcbf711480cc7f1f10462af276cb00466c12d
babrovka <babrovka@gmail.com>
2012-09-29 22:09:53 +0400

    gemfile81

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,8 +46,6 @@ set :application,     "imsat"
 # Сервер размещения проекта.
 set :deploy_server,   "neon.locum.ru"
 
-sunspot:solr:run
-end
 
 # Не включать в поставку разработческие инструменты и пакеты тестирования.
 set :bundle_without,  [:development, :test]
