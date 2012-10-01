6e5bfb6b4f7ebebe42b0847528d5889d70f4cb1f
babrovka <babrovka@gmail.com>
2012-10-01 18:44:54 +0400

    assea2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ Imsat::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
   
-  config.assets.precompile += Ckeditor.assets
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
