1720300387729571ffbe09fd5a466536f892dccf
babrovka <babrovka@gmail.com>
2012-10-01 18:07:56 +0400

    about2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Imsat::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
+  
+  config.assets.precompile += Ckeditor.assets
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
