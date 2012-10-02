099abb84b2c2c75b408b6f072aaa8b216c7adbd0
babrovka <babrovka@gmail.com>
2012-10-02 22:10:59 +0400

    add ser3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,3 +63,15 @@
 			})
 		
 		//  Актив
+		
+		// поиск
+		  (function() {
+		    var cx = '013515421039703036040:utivrm-ynkm';
+		    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+		    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+		        '//www.google.com/cse/cse.js?cx=' + cx;
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+		  })();
+			//  Поиск
+		
+		
