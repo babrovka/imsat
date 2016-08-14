cbb333bdf66db1d5628c7c700a5529a3e2a1c93b
Dragon Black <db87987@gmail.com>
2016-08-14 17:38:42 +0300

    Pretty up js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,61 +17,40 @@
 //= require ckeditor/ckeditor
 //= require ckeditor/init
 
-
-
-
-		// Слайдер
-		$(function() {
-			$("#slider ul").carouFredSel({
-	    		circular: true,
-	    		infinite: false,
-	    		auto 	: {
-					pauseDuration:5000
-				},
-	    		scroll  : {
-	        		items   : 1
-	    		},
-	    	
-	    		pagination  : {
-	    	
-	        		container : "#paging"	
-	        	}
-			});
-		});
-		// Слайдер
+// Слайдер
+$(function() {
+	$("#slider ul").carouFredSel({
+		circular: true,
+		infinite: false,
+		auto: {
+			pauseDuration:5000
+		},
+		scroll: {
+			items: 1
+		},
+		pagination  : {
+			container : "#paging"	
+		}
+	});
+});
 		
-		
-			// Актив	
-			$(function(){
-			function stripTrailingSlash(str) {
-			if(str.substr(-1) == '/') {
-			    return str.substr(0, str.length - 1);
-			}
-			return str;
-			 }
+// Актив	
+$(function(){
+	function stripTrailingSlash(str) {
+		if(str.substr(-1) == '/') {
+			return str.substr(0, str.length - 1);
+		}
+		return str;
+	}
 
-			 var url = window.location.pathname;  
-			var activePage = stripTrailingSlash(url);
+	var url = window.location.pathname;  
+	var activePage = stripTrailingSlash(url);
 
-			 $('.lefttext a').each(function(){  
-			var currentPage = stripTrailingSlash($(this).attr('href'));
+	$('.lefttext a').each(function(){  
+		var currentPage = stripTrailingSlash($(this).attr('href'));
 
-			if (activePage == currentPage) {
+		if (activePage == currentPage) {
 			$(this).parent().addClass('active'); 
-			} 
-			});
-			})
-		
-		//  Актив
-		
-		// поиск
-		  (function() {
-		    var cx = '013515421039703036040:utivrm-ynkm';
-		    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-		    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-		        '//www.google.com/cse/cse.js?cx=' + cx;
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-		  })();
-			//  Поиск
-		
-		
+		} 
+	});
+});
