a7adcecdbc9e61b1d530a504f36e6cd0b275c13e
Dragon Black <db87987@gmail.com>
2016-08-14 15:57:26 +0300

    Fixes repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :unicorn_pid,    "/var/run/unicorn/#{fetch(:user)}/" \
 set :bundle_without, %w{development test}.join(' ')             # this is default
 set :use_sudo,       false
 
-set :repo_url, 'git@example.com:db87987/imsat.git'
+set :repo_url, 'git@github.com:db87987/imsat.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
