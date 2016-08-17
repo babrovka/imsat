7cb415e712b6bc89e5974c7659d187dc5970dd98
Dragon Black <db87987@gmail.com>
2016-08-17 15:54:34 +0300

    Fixes ckeditor uploads

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,10 +1,13 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
-  has_attached_file :data
-  
-  validates_attachment_size :data, :less_than => 100.megabytes
+  has_attached_file :data,
+                    :url => "/system/:class/:attachment/:id_partition/:style/:filename",
+                    :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
+
   validates_attachment_presence :data
-	
-	def url_thumb
-	  @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-	end
+  validates_attachment_size :data, :less_than => 100.megabytes
+  do_not_validate_attachment_file_type :data
+
+  def url_thumb
+    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+  end
 end
